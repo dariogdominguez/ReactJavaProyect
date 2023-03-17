3ff2d774c60be7c7a448404622e8b4f8d749b9a3
Juan Ciancio <juan.ciancio@esolutions.com.ar>
2023-03-17 09:08:15 -0300

    first commit

---
+++ src/main/java/com/dario/fullstackproyect/FullStackProyectApplication.java
@@ -0,0 +1,13 @@
+package com.dario.fullstackproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dario/fullstackproyect/FullStackProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dario.fullstackproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
